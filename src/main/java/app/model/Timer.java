b59130bcae8813220619a33e36eb789763734c36
alikkaribov <alik_karibov@mail.ru>
2022-10-17 23:51:00 +0400

    Удалил созданную сущность Timer

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -3,7 +3,6 @@ package app.model;
 import org.springframework.stereotype.Component;
 @Component
 public class Timer {
-
     private Long nanoTime = System.nanoTime();
     public Long getTime() {
         return nanoTime;
